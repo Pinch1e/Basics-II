4973d76190677273406e7bfc5e0d21f9c5c38ebd
Emmanuel Arihilam <113314419+Pinch1e@users.noreply.github.com>
2025-03-19 16:33:10 +0100

    Add files via upload

---
+++ calculations.py
@@ -0,0 +1,14 @@
+print(int(35.55))
+
+#Quotes inside Quotes
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+#Strings are Arrays
+a = "Hello World"
+print(a[1])
+
+#Looping through a String
+for x in banana :
+    print(x)
